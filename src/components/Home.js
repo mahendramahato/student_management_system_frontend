a08633f3677d89146ea274da4bdc2e43f6b5464a
mahendramahato <mahendramahato33@gmail.com>
2022-09-13 14:09:47 -0500

    routing changes for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,21 +3,21 @@ import {Link} from 'react-router-dom'
 
 export const Home = () => {
 
-  return (
-    <div className="container">
-        <div className="row justify-content-center">
-            <div className="col-md-2">
-                <div className="card shadow" style={{paddingTop:30, paddingLeft:20, paddingRight:20, marginTop:15}}>
-                    <img src="/images/manager.png" className="card-img" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title text-center">Admin</h5>
-                        <div className="row">
-                        <Link to="/admin_login" className="btn btn-primary">Login</Link>
-                        </div>
+return (
+<div className="container">
+    <div className="row justify-content-center">
+        <div className="col-md-2">
+            <div className="card shadow" style={{paddingTop:30, paddingLeft:20, paddingRight:20, marginTop:15}}>
+                <img src="/images/manager.png" className="card-img" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title text-center">Admin</h5>
+                    <div className="row">
+                    <Link to="/admin_login" className="btn btn-primary">Login</Link>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-  )
+</div>
+)
 }
